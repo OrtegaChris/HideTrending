b56bb448c45d5d016665b270b17ecda38bb906a2
OrtegaChris <ortega_chris@outlook.com>
2016-09-09 22:49:32 -0400

    Update Hide Facebook Trending Feed .js

--- Hide Facebook Trending Feed .js
+++ Hide Facebook Trending Feed .js
@@ -1,8 +1,17 @@
-(function() {
+(function()  {
     'use strict';
-	// For Use with Tampermonkey userscriptmanager for Chrome or similar browser script manager
-    var egoPane = document.getElementById("pagelet_ego_pane");
-	var trendsPane = document.getElementById("pagelet_trending_tags_and_topics");
-	trendsPane.style.display = 'none';
-	egoPane.style.display = 'none';
+
+    // Your code here...
+    $(document).ready(function() {
+        hideTrend();
+  });
 })();
+
+
+function hideTrend(){
+		// Hides Trending topics and Suggested Pages
+		var egoPane = document.getElementById("pagelet_ego_pane");
+		var trendsPane = document.getElementById("pagelet_trending_tags_and_topics");
+		trendsPane.style.display = 'none';
+		egoPane.style.display = 'none';
+	}
